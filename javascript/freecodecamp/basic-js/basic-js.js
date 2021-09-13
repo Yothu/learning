a8d2f6fea4fe01d0e3e34debfc58b9e454518304
Yothu <david.vergaray.a@gmail.com>
2021-09-13 20:08:35 -0300

    Basic math js

--- javascript/freecodecamp/basic-js/basic-js.js
+++ javascript/freecodecamp/basic-js/basic-js.js
@@ -5,27 +5,36 @@ a multi-line
 comment */
 
 /* BASIC VARIABLE ASSIGNMENT */
+var myName;                 // Create variables the old way
+let ourName;                // The New way
 
-// Create variables the old way
-var myName;
-
-
-// Asign a variable a value after creation
-var a;
+var a;                      // Asign a variable a value after creation
 a = 7;
 
-// Asign a variable value to other variables
-var b;
+var b;                      // Asign a variable value to other variables
 b = a;
 
-// Create a variable with a value already in it
-var c = 12;
+var c = 12;                 // Create a variable with a value already in it
 
-// Variables without a value are UNDEFINED
-var d; // d id Undefined
+var d;                      // Variables without a value are UNDEFINED // d id Undefined
 
-// Variable names must follow a defined standard
-var someVariable;
+var someVariable;           // Variable names must follow a defined standard
 var anotherVariableName;
 
-/*  */
+/* BASIC MATH */
+var aa = 10 + 34;   // Addition
+var ab = 34 - 12;   // Substraction
+var ac = 12 * 2;    // Multiplication
+var ad = 30 / 3;    // Division
+
+aa++;               // Self addition by 1
+ab--;               // Self substraction by 1
+
+aa += 5;            // Self addition
+ab -= 6;            // Self substraction
+ac *= 2;            // Self multiplication
+ad /= 2;            // Self division
+
+var ae = 3.4;       // Decimals
+
+var remainder = 11 % 3; // Remainder of a division
