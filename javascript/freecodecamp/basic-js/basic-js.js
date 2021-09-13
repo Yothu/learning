9aa958162070de21eb1f4c5214446ea3ff8d2f3f
Yothu <david.vergaray.a@gmail.com>
2021-09-13 17:27:23 -0300

    Basic JavaScript variable assignment

--- javascript/freecodecamp/basic-js/basic-js.js
+++ javascript/freecodecamp/basic-js/basic-js.js
@@ -4,7 +4,7 @@
 a multi-line
 comment */
 
-
+/* BASIC VARIABLE ASSIGNMENT */
 
 // Create variables the old way
 var myName;
@@ -13,3 +13,19 @@ var myName;
 // Asign a variable a value after creation
 var a;
 a = 7;
+
+// Asign a variable value to other variables
+var b;
+b = a;
+
+// Create a variable with a value already in it
+var c = 12;
+
+// Variables without a value are UNDEFINED
+var d; // d id Undefined
+
+// Variable names must follow a defined standard
+var someVariable;
+var anotherVariableName;
+
+/*  */
