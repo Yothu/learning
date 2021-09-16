16f147c2607965f17701a563f20263ae7f488ca6
Yothu <david.vergaray.a@gmail.com>
2021-09-16 00:35:19 -0300

    Intermediate freecodecamp Challenge - Convert HTML Entities

--- javascript/freecodecamp/interAlg/challenge.js
+++ javascript/freecodecamp/interAlg/challenge.js
@@ -45,9 +45,9 @@ function translatePigLatin(str) {
     }
     str = fromArrayToString(arr, aux);
     return str;
-  }
+}
   
-  function fromArrayToString(arr, aux){
+function fromArrayToString(arr, aux){
     let str = "";
     for(let i = 0;i < arr.length;i++){
       str += arr[i];
@@ -58,17 +58,17 @@ function translatePigLatin(str) {
       str += "way";
     }
     return str;
-  }
+}
   
-  function fromStringToArray(str){
+function fromStringToArray(str){
     let arr = [];
     for(let i = 0;i<str.length;i++){
       arr.push(str[i]);
     }
     return arr;
-  }
+}
   
-  function isVowel(char){
+function isVowel(char){
     const vowels = ["a", "e", "i", "o", "u"];
     for(let i = 0; i < vowels.length;i++){
       if(char == vowels[i]){
@@ -76,6 +76,84 @@ function translatePigLatin(str) {
       }
     }
     return false;
-  }
+}
   
-  translatePigLatin("consonant");
+translatePigLatin("consonant");
+
+/* DNA PAIRING */
+//  https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/dna-pairing
+
+function pairElement(str) {
+    let array = [];
+    for(let i = 0;i<str.length;i++){
+      let aux = [str[i]];
+      aux.push(pairElem(str[i]));
+      array.push(aux);
+    }
+    return array;
+}
+  
+function pairElem(char){
+    switch(char){
+      case "A":
+        return "T"
+      case "T":
+        return "A"
+      case "C":
+        return "G"
+      case "G":
+        return "C"
+      default:
+        return null;
+    }
+}
+
+pairElement("GCG");
+
+/* CONVERT HTML ENTITIES */
+//  https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/convert-html-entities
+
+function convertHTML(str) {
+    let arr = fromStringToArray(str);
+    for(let i = 0;i < arr.length;i++){
+      arr[i] = changeEntities(arr[i]);
+    }
+    return fromArrayToString(arr);
+}
+  
+function fromArrayToString(arr){
+    let str = "";
+    for(let i = 0;i < arr.length;i++){
+        str += arr[i];
+    }
+    return str;
+}
+  
+function fromStringToArray(str){
+    let arr = [];
+    for(let i = 0;i<str.length;i++){
+        arr.push(str[i]);
+    }
+    return arr;
+}
+  
+function changeEntities(char){
+    switch (char){
+      case "&":
+        return "&amp;";
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case '"':
+        return "&quot;";
+      case "'":
+        return "&apos;"
+      default:
+        return char;
+    }
+}
+  
+  convertHTML("Dolce & Gabbana");
+
+  
